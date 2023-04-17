8bdecb0e655774a3d1c57a0a41438f0ef128f7c4
Luis Simosa <simosa37@gmail.com>
2023-04-17 12:04:33 -0300

    featured and nearyou card data fetching

--- src/components/featuredCard/featuredCard.jsx
+++ src/components/featuredCard/featuredCard.jsx
@@ -4,20 +4,84 @@ import CalendarLogo from "../calendarLogo/calendarLogo";
 import CheckLogo from "../checkLogo/checkLogo";
 import WheelChairLogo from "../wheelChairLogo/wheelChairLogo";
 
+import axios from "axios";
+import { useQuery } from "@tanstack/react-query";
+import Image from "next/image";
+
 function FeaturedCard() {
+  const { data, isLoading, error } = useQuery({
+    queryKey: ["featured"],
+    queryFn: async () => {
+      const response = await axios
+        .get(
+          "https://inclusive-001-site1.atempurl.com/api/establishment/a9870b9d-f5d5-4281-af4d-08db3edbef24"
+        )
+        .then((res) => res);
+      return response.data;
+    },
+  });
+
+  if (error) {
+    return <h1>Ha ocurrido un error: {error.message}</h1>;
+  }
+
+  if (isLoading) {
+    return (
+      <div className={styles.featuredCard}>
+        <div className={styles.header}>
+          <h2>DESTACADO</h2>
+          <div className={styles.img}></div>
+        </div>
+        <div className={styles.text}>
+          <h3>Cargando...</h3>
+          <div className={styles.address}>
+            <div>
+              <AddressLogo />
+            </div>
+            <p>Cargando...</p>
+          </div>
+          <div className={styles.time}>
+            <div>
+              <CalendarLogo />
+            </div>
+            <p>Abierto. 11:30am - 11:00pm</p>
+          </div>
+          <div className={styles.mobilityTag}>
+            <div className={styles.mobilityTag__title}>
+              <WheelChairLogo />
+              <p>Movilidad Reducida</p>
+            </div>
+
+            <ul>
+              {[...Array(3)].map((e) => {
+                return (
+                  <li key={e?.id}>
+                    <CheckLogo /> <p>{e?.name}</p>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
   return (
     <div className={styles.featuredCard}>
       <div className={styles.header}>
         <h2>DESTACADO</h2>
-        <div className={styles.img}></div>
+        <div className={styles.img}>
+          <Image src={data?.image} alt="photo" width={296} height={178}  />
+        </div>
       </div>
       <div className={styles.text}>
-        <h3>LOS PLATITOS</h3>
+        <h3>{data?.name}</h3>
         <div className={styles.address}>
           <div>
             <AddressLogo />
           </div>
-          <p>Av. Rafael Obligado 1750</p>
+          <p>{data?.address}</p>
         </div>
         <div className={styles.time}>
           <div>
@@ -32,17 +96,13 @@ function FeaturedCard() {
           </div>
 
           <ul>
-            <li>
-              <CheckLogo /> <p>Pasillos amplios</p>
-            </li>
-            <li>
-              <CheckLogo />
-              <p>Baños acondicionados</p>
-            </li>
-            <li>
-              <CheckLogo />
-              <p>Antideslizante y barras de apoyo</p>
-            </li>
+          {data?.accessibilitys?.map((e) => {
+                return (
+                  <li key={e?.id}>
+                    <CheckLogo /> <p>{e?.name}</p>
+                  </li>
+                );
+              })}
           </ul>
         </div>
       </div>
--- src/components/nearYouCard/nearYouCard.jsx
+++ src/components/nearYouCard/nearYouCard.jsx
@@ -4,20 +4,85 @@ import CalendarLogo from "../calendarLogo/calendarLogo";
 import WheelChairLogo from "../wheelChairLogo/wheelChairLogo";
 import CheckLogo from "../checkLogo/checkLogo";
 
+import axios from "axios";
+import { useQuery } from "@tanstack/react-query";
+import Image from "next/image";
+
 function NearYouCard() {
+  const { data, isLoading, error } = useQuery({
+    queryKey: ["nearyou"],
+    queryFn: async () => {
+      const response = await axios
+        .get(
+          "https://inclusive-001-site1.atempurl.com/api/establishment/0d62648d-7311-4050-af4b-08db3edbef24"
+        )
+        .then((res) => res);
+      return response.data;
+    },
+  });
+
+  if (error) {
+    return <h1>Ha ocurrido un error: {error.message}</h1>;
+  }
+
+  if (isLoading) {
+    return (
+      <div className={styles.featuredCard}>
+        <div className={styles.header}>
+          <h2>CERCA DE TU UBICACIÓN</h2>
+          <div className={styles.img}></div>
+        </div>
+        <div className={styles.text}>
+          <h3>Cargando...</h3>
+          <div className={styles.address}>
+            <div>
+              <AddressLogo />
+            </div>
+            <p>Cargando...</p>
+          </div>
+          <div className={styles.time}>
+            <div>
+              <CalendarLogo />
+            </div>
+            <p>Cargando...</p>
+          </div>
+
+          <div className={styles.mobilityTag}>
+            <div className={styles.mobilityTag__title}>
+              <WheelChairLogo />
+              <p>Movilidad Reducida</p>
+            </div>
+
+            <ul>
+              {data?.accessibilitys?.map((e) => {
+                return (
+                  <li key={e.id}>
+                    <CheckLogo /> <p>{e.name}</p>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
   return (
     <div className={styles.featuredCard}>
       <div className={styles.header}>
         <h2>CERCA DE TU UBICACIÓN</h2>
-        <div className={styles.img}></div>
+        <div className={styles.img}>
+          <Image src={data?.image} alt="" width={296} height={178} />
+        </div>
       </div>
       <div className={styles.text}>
-        <h3>PARQUE SAN FRANCISCO</h3>
+        <h3>{data?.name}</h3>
         <div className={styles.address}>
           <div>
             <AddressLogo />
           </div>
-          <p>San Francisco 2120, B1609 Boulogne</p>
+          <p>{data?.address}</p>
         </div>
         <div className={styles.time}>
           <div>
@@ -33,20 +98,15 @@ function NearYouCard() {
           </div>
 
           <ul>
-            <li>
-              <CheckLogo /> <p>Pasillos amplios</p>
-            </li>
-            <li>
-              <CheckLogo />
-              <p>Baños acondicionados</p>
-            </li>
-            <li>
-              <CheckLogo />
-              <p>Antideslizante y barras de apoyo</p>
-            </li>
+            {data?.accessibilitys?.map((e) => {
+              return (
+                <li key={e?.id}>
+                  <CheckLogo /> <p>{e?.name}</p>
+                </li>
+              );
+            })}
           </ul>
         </div>
-
       </div>
     </div>
   );
