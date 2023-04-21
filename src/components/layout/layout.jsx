d4b9541b2e8378c0e5dd329e0b09972fbe044de0
Luis Simosa <simosa37@gmail.com>
2023-04-21 14:58:32 -0300

    fix header bug

--- src/components/layout/layout.jsx
+++ src/components/layout/layout.jsx
@@ -5,7 +5,7 @@ function MainLayout({ children }) {
 
   //
   const router = useRouter();
-  const routes = ["/login", "/signup", "/signIn", "/landing", "/register"]
+  const routes = ["/login", "/signup", "/signIn", "/landing", "/frecuent", "/register"]
 
   return (
     <>
